65425a3e0449fee11c9b68cb1e49627929f1297f
hmk10 <halitmk@gmail.com>
2021-12-07 21:54:48 -0500

    Abstraction

---
+++ src/Day33/BankCardPackage/DebitCard.java
@@ -0,0 +1,24 @@
+package Day33.BankCardPackage;
+
+public class DebitCard extends BankCard {
+
+    public DebitCard(String name, int SSN) {
+        super(name, SSN);
+    }
+
+    @Override
+    public void deposit(double moneyToDeposit) {
+        if (moneyToDeposit <= 0)
+            throw new IllegalArgumentException("You cannot deposit this amount!");
+        setBalance(getBalance() + moneyToDeposit);
+    }
+
+    @Override
+    public void withdraw(double moneyToWithdraw) {
+        if (moneyToWithdraw <= 0)
+            throw new IllegalArgumentException("You cannot withdraw this amount!");
+        if (getBalance() < moneyToWithdraw)
+            throw new IllegalArgumentException("You cannot withdraw this amount!");
+        setBalance(getBalance() - moneyToWithdraw);
+    }
+}
