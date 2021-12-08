9dd2e68e58863559a28e51ad9319df72239f9a71
hmk10 <halitmk@gmail.com>
2021-12-07 20:42:43 -0500

    Abstraction

---
+++ src/Day33/AbstractKeyword/Child.java
@@ -0,0 +1,10 @@
+package Day33.AbstractKeyword;
+
+public abstract class Child extends Parent {
+
+    @Override
+    public void abstractMethod1() {
+        System.out.println("Implemented!");
+    }
+
+}
---
+++ src/Day33/AbstractKeyword/Parent.java
@@ -0,0 +1,9 @@
+package Day33.AbstractKeyword;
+
+public abstract class Parent {
+
+    public abstract void abstractMethod1();
+
+    public abstract void abstractMethod2();
+
+}
