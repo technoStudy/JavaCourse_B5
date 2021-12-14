20be532e3978a63f2ca08d2273462c9ff6ee0770
hmk10 <halitmk@gmail.com>
2021-12-13 21:40:33 -0500

    ArrayList - Task

--- src/Day36/_07_ArrayListTask.java
+++ src/Day36/_07_ArrayListTask.java
@@ -1,9 +1,31 @@
 package Day36;
 
+import java.util.ArrayList;
+
 public class _07_ArrayListTask {
 
+    // Part1:
     // create an arraylist of Strings
     // add - "Red", "Green", "Blue", "Yellow" and "Black" inside
     // print out all the elements, each element on new line
+
+    // Part2:
+    // Create a method that accepts a String arraylist and a String as parameters
+    // method should add the element at the first index of arraylist
+
+    public static void main(String[] args) {
+
+        ArrayList<String> colors = new ArrayList<>();
+
+        colors.add("Red");
+        colors.add("Green");
+        colors.add("Blue");
+        colors.add("Yellow");
+        colors.add("Black");
+
+        for (int i = 0; i < colors.size(); i++)
+            System.out.println(colors.get(i));
+
+    }
 
 }
