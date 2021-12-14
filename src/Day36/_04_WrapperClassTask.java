a7fb7fc672355b1373c7b5282e4514cf2c275139
hmk10 <halitmk@gmail.com>
2021-12-13 20:10:53 -0500

    Wrapper Class

--- src/Day36/_04_WrapperClassTask.java
+++ src/Day36/_04_WrapperClassTask.java
@@ -3,5 +3,26 @@ package Day36;
 public class _04_WrapperClassTask {
 
     // create variables of all types of Wrapper classes and give them a value
+    // convert these variables into primitive
+
+    public static void main(String[] args) {
+
+        Byte byteWrapper = 5;
+
+        Short shortWrapper = 35;
+
+        Integer intWrapper = 30000;
+
+        Long longWrapper = 9876547654L;
+
+        Boolean booleanWrapper = true;
+
+        Character charWrapper = 'T';
+
+        Float floatWrapper = 5.95F;
+
+        Double doubleWrapper = 95.765;
+
+    }
 
 }
