13ed824550607ece90c7b20a683f8b145f7533d9
hmk10 <halitmk@gmail.com>
2021-12-13 21:53:56 -0500

    ArrayList - Task

--- src/Day36/_07_ArrayListTask.java
+++ src/Day36/_07_ArrayListTask.java
@@ -5,13 +5,18 @@ import java.util.ArrayList;
 public class _07_ArrayListTask {
 
     // Part1:
-    // create an arraylist of Strings
+    // Create an arraylist of Strings
     // add - "Red", "Green", "Blue", "Yellow" and "Black" inside
     // print out all the elements, each element on new line
 
     // Part2:
     // Create a method that accepts a String arraylist and a String as parameters
     // method should add the element at the first index of arraylist
+
+    // Part3:
+    // Create a method that accepts a String arraylist and a String as parameters
+    // it should add element before last element
+    // ex:  [1, 2, 3]  -> [1, 2, 4, 3]
 
     public static void main(String[] args) {
 
@@ -26,6 +31,13 @@ public class _07_ArrayListTask {
         for (int i = 0; i < colors.size(); i++)
             System.out.println(colors.get(i));
 
+        addElementAtIndex0(colors, "White");
+        System.out.println(colors);
+
+    }
+
+    private static void addElementAtIndex0(ArrayList<String> list, String element) {
+        list.add(0, element);
     }
 
 }
