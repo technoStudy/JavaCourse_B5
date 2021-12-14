13a89174c2c2bcdadebc756128f13fb852f618fd
hmk10 <halitmk@gmail.com>
2021-12-13 19:34:09 -0500

    Wrapper Class

---
+++ src/Day36/_02_AutoBoxingAndUnBoxing.java
@@ -0,0 +1,16 @@
+package Day36;
+
+public class _02_AutoBoxingAndUnBoxing {
+
+    public static void main(String[] args) {
+
+        byte primitiveVariable = 10;
+        Byte referenceVariable = primitiveVariable; // autoboxing
+
+
+        Character referenceChar = 'A';
+        char primitiveChar = referenceChar; // unboxing
+
+    }
+
+}
---
+++ src/Day36/_03_WrapperClassTask.java
@@ -0,0 +1,10 @@
+package Day36;
+
+public class _03_WrapperClassTask {
+
+    // create a method that accepts an int
+    // and returns twice of that int
+
+    // overload this method with wrapper class: Integer
+
+}
