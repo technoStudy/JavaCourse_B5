ca2e97bf9cdb993bdb9fd150abc4c1102c4438a3
hmk10 <halitmk@gmail.com>
2021-12-13 19:47:17 -0500

    Wrapper Class

--- src/Day36/_03_WrapperClassTask.java
+++ src/Day36/_03_WrapperClassTask.java
@@ -7,4 +7,22 @@ public class _03_WrapperClassTask {
 
     // overload this method with wrapper class: Integer
 
+    public static void main(String[] args) {
+
+        System.out.println(twice(5));
+
+        Integer number = 10;
+        System.out.println(twice(number));
+
+    }
+
+
+    public static int twice(int number) {
+        return number * 2;
+    }
+
+    public static Integer twice(Integer number) {
+        return number * 2;
+    }
+
 }
