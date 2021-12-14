a1e136a764857bd2bd54041d1956b6ffd32361bb
hmk10 <halitmk@gmail.com>
2021-12-13 20:55:17 -0500

    Array vs ArrayList

---
+++ src/Day36/_05_ArrayVsArrayList.java
@@ -0,0 +1,21 @@
+package Day36;
+
+public class _05_ArrayVsArrayList {
+
+    /**
+
+                    Array VS ArrayList
+
+        - Array is a fixed-length data structure, so we can't change length of Array
+          once created. ArrayList is resizable (Dynamic Array)
+
+        - Array can contain both primitives and objects
+          ArrayList can only store Objects
+
+        - Also we can compare Array and ArrayList on how to calculate length of
+          Array or size of ArrayList
+          We use length() method for arrays, size() method for ArrayList
+
+     */
+
+}
