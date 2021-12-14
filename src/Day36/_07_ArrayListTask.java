866d228c960edb0644feb2ce0c68a4eeccb4a57c
hmk10 <halitmk@gmail.com>
2021-12-13 21:28:35 -0500

    ArrayList - Task

---
+++ src/Day36/_07_ArrayListTask.java
@@ -0,0 +1,9 @@
+package Day36;
+
+public class _07_ArrayListTask {
+
+    // create an arraylist of Strings
+    // add - "Red", "Green", "Blue", "Yellow" and "Black" inside
+    // print out all the elements, each element on new line
+
+}
