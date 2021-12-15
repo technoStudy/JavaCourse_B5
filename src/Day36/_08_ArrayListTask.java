00c5a1f8f5f8e80903c43de64306dea11ea59cfd
hmk10 <halitmk@gmail.com>
2021-12-14 19:34:47 -0500

    ArrayList - Task

---
+++ src/Day36/_08_ArrayListTask.java
@@ -0,0 +1,8 @@
+package Day36;
+
+public class _08_ArrayListTask {
+
+    // create a method that will create an arrayList of integers
+    // with 5 random numbers(1 to 10) and return the created ArrayList
+
+}
