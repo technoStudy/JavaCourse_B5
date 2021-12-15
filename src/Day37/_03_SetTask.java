98dc27e85631ac9666f02a2f7739bd5ce4a5484b
hmk10 <halitmk@gmail.com>
2021-12-14 21:27:38 -0500

    Set Task

--- src/Day37/_03_SetTask.java
+++ src/Day37/_03_SetTask.java
@@ -1,5 +1,8 @@
 package Day37;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class _03_SetTask {
 
     // Task
@@ -7,8 +10,21 @@ public class _03_SetTask {
     // create a String[] array of student name in your group, duplicate some names
     // add all the elements from String[] to Set
 
+    // Part 2:
+    // create a method that takes set and varargs of String and adds all elements to the set
+    // public static void addAll(HashSet<String> set, Strings... elements) {}
+
+
     public static void main(String[] args) {
 
+        String[] names = {"Adem", "Matthew", "Lana", "Betul", "Amsal", "Adem", "Lana"};
+
+        Set<String> namesSet = new HashSet<>();
+
+        for (int i = 0; i < names.length; i++)
+            namesSet.add(names[i]);
+
+        System.out.println(namesSet);
 
 
     }
