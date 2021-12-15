2004da3c334311db60a65716e088287b6a3513cf
hmk10 <halitmk@gmail.com>
2021-12-14 21:14:32 -0500

    Set Task

--- src/Day37/_03_SetTask.java
+++ src/Day37/_03_SetTask.java
@@ -2,7 +2,15 @@ package Day37;
 
 public class _03_SetTask {
 
+    // Task
+    // Part 1:
     // create a String[] array of student name in your group, duplicate some names
     // add all the elements from String[] to Set
+
+    public static void main(String[] args) {
+
+
+
+    }
 
 }
