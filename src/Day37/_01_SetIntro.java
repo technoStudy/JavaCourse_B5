37defbb90be9a669628ddd993b06ae7abe8c615f
hmk10 <halitmk@gmail.com>
2021-12-14 20:32:26 -0500

    Set

---
+++ src/Day37/_01_SetIntro.java
@@ -0,0 +1,17 @@
+package Day37;
+
+public class _01_SetIntro {
+
+    /**
+
+        SET
+          - It's an unordered collection of objects.
+          - Set cannot store duplicate objects.
+
+        ArrayList VS Set
+          - List can have duplicate elements but Set cannot have duplicate elements.
+          - In List elements are stored in insertion order. Elements in Set is stored like groups.
+
+     */
+
+}
