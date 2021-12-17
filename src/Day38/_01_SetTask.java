220d3e3e2af02148363478b6048f58f6734ab524
hmk10 <halitmk@gmail.com>
2021-12-16 19:08:19 -0500

    Set - Task Recap

---
+++ src/Day38/_01_SetTask.java
@@ -0,0 +1,10 @@
+package Day38;
+
+public class _01_SetTask {
+
+    // create a method that accepts ArrayList<Integer> and Set<Integer> as parameters
+    // and add elements all elements from ArrayList to Set
+
+
+
+}
