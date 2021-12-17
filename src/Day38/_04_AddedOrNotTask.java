7ca17820693f196a9e241c8a94067fc0ce31f861
hmk10 <halitmk@gmail.com>
2021-12-16 20:15:07 -0500

    Set - add() method return type - Task

---
+++ src/Day38/_04_AddedOrNotTask.java
@@ -0,0 +1,10 @@
+package Day38;
+
+public class _04_AddedOrNotTask {
+
+    // Task
+    // create a list of numbers 1, 3, 1, 2, 4, 5, 3 --> 2 duplicates
+    // count how many duplicates we have
+    // find out what are the duplicated values
+
+}
