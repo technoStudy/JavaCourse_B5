cfd78121a2edee6607e05dfd770d365fb8ba964d
hmk10 <halitmk@gmail.com>
2021-12-16 19:37:46 -0500

    Set - Task Recap

--- src/Day38/_01_SetTask.java
+++ src/Day38/_01_SetTask.java
@@ -6,9 +6,11 @@ import java.util.Set;
 
 public class _01_SetTask {
 
+    // PART 1
     // create a method that accepts ArrayList<Integer> and Set<Integer> as parameters
     // and add all elements from ArrayList to Set
 
+    // PART 2
     // create a method that takes Set<Character> and varargs of char as parameters
     // and adds all chars to the set
 
@@ -33,6 +35,13 @@ public class _01_SetTask {
         // This set will contain only unique values from the arrayList
         System.out.println("Set after using convert method: " + set);
 
+
+        Set<Character> characterSet = new HashSet<>();
+
+        addCharactersToSet(characterSet, 'A', 'F', 'r', 'R', 'Q');
+
+        System.out.println("Character Set: " + characterSet);
+
     }
 
     // With this method we're converting Arraylist into Set
@@ -40,6 +49,12 @@ public class _01_SetTask {
     public static void convertArrayListIntoSet(ArrayList<Integer> list, Set<Integer> set) {
         for (Integer number : list)
             set.add(number);
+    }
+
+
+    public static void addCharactersToSet(Set<Character> set, Character... chars) {
+        for (int i = 0; i < chars.length; i++)
+            set.add(chars[i]);
     }
 
 }
