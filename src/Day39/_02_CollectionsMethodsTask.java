d6b821b1675fd34593476244cbaed34a6857cbb5
hmk10 <halitmk@gmail.com>
2021-12-17 21:02:23 -0500

    Collections class methods - Task

--- src/Day39/_02_CollectionsMethodsTask.java
+++ src/Day39/_02_CollectionsMethodsTask.java
@@ -1,5 +1,9 @@
 package Day39;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class _02_CollectionsMethodsTask {
 
     // Task
@@ -11,5 +15,38 @@ public class _02_CollectionsMethodsTask {
 
     // part 2: find the max number and min number, print them
     // replace all max number with min number
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Enter number: ");
+            arrayList.add(scanner.nextInt());
+        }
+
+        System.out.println("This is our ArrayList: " + arrayList);
+
+        Collections.sort(arrayList, Collections.reverseOrder());
+        System.out.println("ArrayList after reverse order: " + arrayList);
+
+        Collections.shuffle(arrayList);
+        System.out.println("ArrayList after shuffle: " + arrayList);
+
+        Collections.rotate(arrayList, 3);
+        System.out.println("ArrayList after rotate by 3: " + arrayList);
+
+        Integer max = Collections.max(arrayList);
+        Integer min = Collections.min(arrayList);
+
+        System.out.println("Max value: " + max);
+        System.out.println("Min value: " + min);
+
+        Collections.replaceAll(arrayList, max, min);
+        System.out.println("ArrayList after replaceAll: " + arrayList);
+
+    }
 
 }
