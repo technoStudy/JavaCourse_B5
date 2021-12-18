e75092a7e353be8ec76189635657753982aa1f65
hmk10 <halitmk@gmail.com>
2021-12-17 20:34:05 -0500

    Collections class methods

---
+++ src/Day39/_02_CollectionsMethodsTask.java
@@ -0,0 +1,15 @@
+package Day39;
+
+public class _02_CollectionsMethodsTask {
+
+    // Task
+    // create list of 5 numbers from user input
+    // sort the list in reverse order and print it
+    // shuffle the list and print it
+    // rotate right by 3 places and print it
+
+
+    // part 2: find the max number and min number, print them
+    // replace all max number with min number
+
+}
