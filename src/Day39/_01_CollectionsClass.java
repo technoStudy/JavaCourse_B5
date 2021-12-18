7750eef79645b67a176273c9e8071612ae7bcaa8
hmk10 <halitmk@gmail.com>
2021-12-17 20:32:21 -0500

    Collections class methods

--- src/Day39/_01_CollectionsClass.java
+++ src/Day39/_01_CollectionsClass.java
@@ -41,9 +41,33 @@ public class _01_CollectionsClass {
         Collections.shuffle(arrayList);
         System.out.println("ArrayList after shuffle method: " + arrayList);
 
-        // rotate() --
-        Collections.rotate(arrayList, 2);
+        // rotate() -- will move all elements with the distance we provide
+        Collections.rotate(arrayList, 2); // positive number will move right, negative will move left
         System.out.println("ArrayList after rotate method: " + arrayList);
+
+        // replaceAll() -- all the values that we provide will be replaced with new value
+        Collections.replaceAll(arrayList, 22, 9999999);
+        System.out.println("ArrayList after replaceAll method: " + arrayList);
+
+        // max() -- to find biggest number in Collection
+        Integer maxValue = Collections.max(arrayList);
+        System.out.println("Max value for ArrayList: " + maxValue);
+
+        // min() -- to find smallest number in Collection
+        Integer minValue = Collections.min(arrayList);
+        System.out.println("Min value for ArrayList: " + minValue);
+
+        // max() and min() methods can be used for Set too!
+        Integer maxValueForSet = Collections.max(hashSet);
+        System.out.println("Max value for HashSet: " + maxValueForSet);
+
+        Integer minValueForSet = Collections.min(hashSet);
+        System.out.println("Min value for HashSet: " + minValueForSet);
+
+        // binarySearch() -- we need to sort List first before using binarySearch
+        Collections.sort(arrayList);
+        int indexOf44 = Collections.binarySearch(arrayList, 44);
+        System.out.println(indexOf44);
 
     }
 
