cc5aff5e31018d1efb2672da70d647b09c367f51
hmk10 <halitmk@gmail.com>
2021-12-20 19:41:27 -0500

    2D Collections - Task

---
+++ src/Day40/_02_2D_Collections_Task.java
@@ -0,0 +1,10 @@
+package Day40;
+
+public class _02_2D_Collections_Task {
+
+    // TASK
+    // Create 2D ArrayList which can store String ArrayLists
+    // Create 3 ArrayLists which are Employees, Employers, Companies
+    // Store this 3 ArrayList in 2D ArrayList
+
+}
