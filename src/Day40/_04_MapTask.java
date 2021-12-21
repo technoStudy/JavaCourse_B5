32e84d91f2d97836fbe0c29dc433e456b24da015
hmk10 <halitmk@gmail.com>
2021-12-20 21:49:40 -0500

    Map - Task

--- src/Day40/_04_MapTask.java
+++ src/Day40/_04_MapTask.java
@@ -1,5 +1,8 @@
 package Day40;
 
+import java.util.HashMap;
+import java.util.Set;
+
 public class _04_MapTask {
 
     // create a map of zipcodes and Cities, at least 3 entries
@@ -8,5 +11,21 @@ public class _04_MapTask {
     // zipcode2 => city2
 
     // part 2: print all the keys of the map
+
+    public static void main(String[] args) {
+
+        HashMap<Integer, String> zipAndCity = new HashMap<>();
+        zipAndCity.put(7010, "Cliffside Park");
+        zipAndCity.put(8536, "Plainsboro");
+        zipAndCity.put(1234, "Brooklyn");
+
+        Set<Integer> keys = zipAndCity.keySet();
+
+        for (Integer key : keys)
+            System.out.println(key + " => " + zipAndCity.get(key));
+
+        System.out.println(keys);
+
+    }
 
 }
