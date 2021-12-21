e32915bb1b1ce934c20927acf2dc318065672517
hmk10 <halitmk@gmail.com>
2021-12-20 21:36:49 -0500

    Map - Task

---
+++ src/Day40/_04_MapTask.java
@@ -0,0 +1,12 @@
+package Day40;
+
+public class _04_MapTask {
+
+    // create a map of zipcodes and Cities, at least 3 entries
+    // get the keySet and iterate over it, print the map like below:
+    // zipcode1 => city1
+    // zipcode2 => city2
+
+    // part 2: print all the keys of the map
+
+}
