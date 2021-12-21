c84cc9110ae1d41c57469014beda92305e6908b6
hmk10 <halitmk@gmail.com>
2021-12-20 19:53:56 -0500

    2D Collections - Task

--- src/Day40/_02_2D_Collections_Task.java
+++ src/Day40/_02_2D_Collections_Task.java
@@ -1,10 +1,36 @@
 package Day40;
 
+import java.util.ArrayList;
+
 public class _02_2D_Collections_Task {
 
     // TASK
     // Create 2D ArrayList which can store String ArrayLists
     // Create 3 ArrayLists which are Employees, Employers, Companies
     // Store this 3 ArrayList in 2D ArrayList
+
+    public static void main(String[] args) {
+
+        ArrayList<ArrayList<String>> listOfLists = new ArrayList<>();
+
+            ArrayList<String> employees = new ArrayList<>();
+            employees.add("Mike");
+            employees.add("Peter");
+            employees.add("Lisa");
+
+            ArrayList<String> employers = new ArrayList<>();
+            employers.add("Rob");
+            employers.add("Alex");
+
+            ArrayList<String> companies = new ArrayList<>();
+            companies.add("Amazon");
+            companies.add("Google");
+
+        listOfLists.add(employees);
+        listOfLists.add(employers);
+        listOfLists.add(companies);
+
+        System.out.println(listOfLists);
+    }
 
 }
