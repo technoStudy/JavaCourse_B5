d64619204610d086760d42b3ca6a7831bf51e9ca
hmk10 <halitmk@gmail.com>
2021-12-21 21:17:12 -0500

    Map - Task

--- src/Day41/_03_MapRecapTask3.java
+++ src/Day41/_03_MapRecapTask3.java
@@ -8,20 +8,8 @@ import java.util.Set;
 public class _03_MapRecapTask3 {
 
 //        TASK
-
 //    Following Map is given:
-
-//    HashMap<String, ArrayList<Integer>> titleWonYears = new HashMap<>();
-//
-//    ArrayList<Integer> barca = new ArrayList<>(Arrays.asList(1990, 1995, 2003, 2020));
-//    ArrayList<Integer> real = new ArrayList<>(Arrays.asList(1992, 2008, 2011, 2012, 2014, 2015));
-//    ArrayList<Integer> juve = new ArrayList<>(Arrays.asList(2005, 2021));
-//
-//        titleWonYears.put("Barcelona", barca);
-//        titleWonYears.put("Real Madrid", real);
-//        titleWonYears.put("Juventus", juve);
-
-//        Find how many titles each team won
+//    Find how many titles each team won
 
     public static void main(String[] args) {
 
