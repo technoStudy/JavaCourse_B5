b8aec0cf446cb912066e908b8e50b2188d37de7a
hmk10 <halitmk@gmail.com>
2021-12-21 19:32:09 -0500

    Map - Task

---
+++ src/Day41/_02_MapRecapTask.java
@@ -0,0 +1,11 @@
+package Day41;
+
+public class _02_MapRecapTask {
+
+    // PART 1
+    // create a map with groupName and groupMembers
+    // "Group1" = ["Member1", "Member2"]
+    // "Group2" = ["Member3", "Member4", "Member5"]
+
+
+}
