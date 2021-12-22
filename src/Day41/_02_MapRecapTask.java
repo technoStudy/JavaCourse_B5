d68aff5c705a72b1d10dfb02dbdd2ffd9aae6bf3
hmk10 <halitmk@gmail.com>
2021-12-21 19:55:08 -0500

    Map - Task

--- src/Day41/_02_MapRecapTask.java
+++ src/Day41/_02_MapRecapTask.java
@@ -1,11 +1,29 @@
 package Day41;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class _02_MapRecapTask {
 
     // PART 1
-    // create a map with groupName and groupMembers
+    // create a map with groupName as key and groupMembers as value
     // "Group1" = ["Member1", "Member2"]
     // "Group2" = ["Member3", "Member4", "Member5"]
+
+    public static void main(String[] args) {
+
+        HashMap<String, ArrayList<String>> groupsAndMembers = new HashMap<>();
+
+        ArrayList<String> devTeam = new ArrayList<>(Arrays.asList("Idriz", "Amsal", "Lana", "Ayna"));
+        ArrayList<String> qaTeam =  new ArrayList<>(Arrays.asList("Shuang", "Yusuf", "Feriha"));
+
+        groupsAndMembers.put("DevelopmentTeam", devTeam);
+        groupsAndMembers.put("QaTeam", qaTeam);
+
+        System.out.println(groupsAndMembers);
+
+    }
 
 
 }
