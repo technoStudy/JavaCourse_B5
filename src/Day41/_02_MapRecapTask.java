ea43b01cb6e90d21599336f168bd76faddb6b559
hmk10 <halitmk@gmail.com>
2021-12-21 20:11:10 -0500

    Map - Task

--- src/Day41/_02_MapRecapTask.java
+++ src/Day41/_02_MapRecapTask.java
@@ -11,12 +11,17 @@ public class _02_MapRecapTask {
     // "Group1" = ["Member1", "Member2"]
     // "Group2" = ["Member3", "Member4", "Member5"]
 
+    // PART 2
+    // find how many members each group has
+
     public static void main(String[] args) {
 
         HashMap<String, ArrayList<String>> groupsAndMembers = new HashMap<>();
 
-        ArrayList<String> devTeam = new ArrayList<>(Arrays.asList("Idriz", "Amsal", "Lana", "Ayna"));
-        ArrayList<String> qaTeam =  new ArrayList<>(Arrays.asList("Shuang", "Yusuf", "Feriha"));
+        ArrayList<String> devTeam =
+                new ArrayList<>(Arrays.asList("Idriz", "Amsal", "Lana", "Ayna"));
+        ArrayList<String> qaTeam =
+                new ArrayList<>(Arrays.asList("Shuang", "Yusuf", "Feriha"));
 
         groupsAndMembers.put("DevelopmentTeam", devTeam);
         groupsAndMembers.put("QaTeam", qaTeam);
