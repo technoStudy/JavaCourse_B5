3fa6c1db0efe8441dd4f99e03d8d4ed8d53c4b38
hmk10 <halitmk@gmail.com>
2021-12-22 20:13:14 -0500

    Task

---
+++ src/Day42/Task/Task1.java
@@ -0,0 +1,20 @@
+package Day42.Task;
+
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter a number: ");
+        String input = scanner.nextLine();
+
+        int number = Integer.parseInt(input);
+        System.out.println(number);
+
+        // handle exception in this code
+
+    }
+
+}
