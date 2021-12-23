df582f16a86c78723087a67795860f6adc69230f
hmk10 <halitmk@gmail.com>
2021-12-22 21:51:18 -0500

    Try - Catch - Finally

---
+++ src/Day42/ThrowsKeyword/_03_TryCatchFinally.java
@@ -0,0 +1,17 @@
+package Day42.ThrowsKeyword;
+
+public class _03_TryCatchFinally {
+
+    public static void main(String[] args) {
+
+        try {
+            // try to do this
+        } catch (Exception ex) {
+            // if there is an exception do this
+        } finally {
+            // do this anyway
+        }
+
+    }
+
+}
