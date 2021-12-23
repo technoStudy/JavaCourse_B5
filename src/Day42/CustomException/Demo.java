666e98ea1e3d95827c6783e1a11efbdf389b0124
hmk10 <halitmk@gmail.com>
2021-12-22 22:00:26 -0500

    Custom Exception

---
+++ src/Day42/CustomException/Demo.java
@@ -0,0 +1,21 @@
+package Day42.CustomException;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 11, 12, 13, 14, 15};
+
+        printArrayElement(arr, 20);
+
+    }
+
+
+    public static void printArrayElement(int[] array, int elementIndex) {
+        if (elementIndex >= array.length) {
+            throw new YouAreWrongBro();
+        }
+        System.out.println(array[elementIndex]);
+    }
+
+}
---
+++ src/Day42/CustomException/YouAreWrongBro.java
@@ -0,0 +1,5 @@
+package Day42.CustomException;
+
+public class YouAreWrongBro extends RuntimeException {
+
+}
