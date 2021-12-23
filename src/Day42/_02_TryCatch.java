2608b5e2ecb1a759041d98d0dbf7131ea6d078be
hmk10 <halitmk@gmail.com>
2021-12-22 20:08:54 -0500

    Try Catch Block

---
+++ src/Day42/_02_TryCatch.java
@@ -0,0 +1,16 @@
+package Day42;
+
+public class _02_TryCatch {
+
+    public static void main(String[] args) {
+
+        try {
+            // try to execute this code
+        }
+        catch (Exception e) {
+            // if there is an exception do this
+        }
+
+    }
+
+}
