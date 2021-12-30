dfd59633eb3e7ee5bae906487867f7f01abd1d2e
hmk10 <halitmk@gmail.com>
2021-12-29 19:35:38 -0500

    Iterator

---
+++ src/Day44/_01_IteratorIntro.java
@@ -0,0 +1,62 @@
+package Day44;
+
+import Day35.FinalClassesAndMethods.Parent;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class _01_IteratorIntro {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+
+        Iterator<String> it = cars.iterator();
+
+        // To get first element we use next()!
+        // System.out.println(it.next());
+
+        // Printing all elements in arrayList using Iterator
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(12);
+        numbers.add(8);
+        numbers.add(2);
+        numbers.add(23);
+        numbers.add(42);
+        numbers.add(9);
+        numbers.add(10);
+
+        Iterator<Integer> iterator = numbers.iterator();
+
+        while (iterator.hasNext()) {
+            Integer number = iterator.next();
+            if (number < 10) {
+                iterator.remove();
+            }
+        }
+
+        System.out.println(numbers);
+
+
+        HashSet<String> names = new HashSet<>();
+        names.add("Mike");
+        names.add("Steven");
+        names.add("Alex");
+        names.add("Robin");
+        names.add("Amanda");
+
+        // Part1: Using iterator print all the elements 1 by 1
+
+
+    }
+
+}
